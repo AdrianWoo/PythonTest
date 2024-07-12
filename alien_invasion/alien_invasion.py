7b1d55aad05f0c462c6df78ed8a275aa27c9c048
AdrianWoo <747218766@qq.com>
2024-07-12 16:14:49 +0800

    Finishi bullet

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -30,9 +30,9 @@ class AlienInvasion:
         """
         while True:
             self._check_events()  # 事件检测
-            self._update_screen()  # 屏幕绘制
             self.ship.update()  # 飞船移动
-            self.bullets.update()  # 子弹位置更新移动
+            self._update_bullets()
+            self._update_screen()  # 屏幕绘制
             # 按照每秒60次的速度运行此函数
             self.clock.tick(60)
 
@@ -56,8 +56,10 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
             sys.exit()
         elif event.key == pygame.K_SPACE:
-            new_bullet = Bullet(self)
-            self.bullets.add(new_bullet)
+            # 创建子弹 允许小于数量的子弹创建
+            if len(self.bullets) < self.settings.bullet_allowed:
+                new_bullet = Bullet(self)
+                self.bullets.add(new_bullet)
 
     def _check_keyup_events(self, event):
         """按键抬起事件"""
@@ -76,6 +78,16 @@ class AlienInvasion:
         self.ship.blitme()
         # 绘制屏幕可见
         pygame.display.flip()
+        
+    def _update_bullets(self):
+        """更新子弹的位置并删除已消失的子弹"""
+        # 更新子弹的位置
+        self.bullets.update()
+        
+         #删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.y <= 0:
+                self.bullets.remove(bullet)
 
 
 if __name__ == "__main__":
