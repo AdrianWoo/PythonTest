d5a022c4a66360cef6a751b4347bf1868b313a47
AdrianWoo <747218766@qq.com>
2024-07-12 09:25:26 +0800

    Read file

---
+++ Chapter10/file_reader.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+path = Path('E:/SourceCode/PythonTest/Chapter10/pi_digits.txt')
+contents = path.read_text()
+lines = contents.splitlines()
+for line in lines:
+    print(line)
