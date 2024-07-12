16c465e5336429b15e88da87475d815e6707d95a
AdrianWoo <747218766@qq.com>
2024-07-12 14:30:46 +0800

    finish ship moving and rim

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """初始化飞船并设置其初始位置"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取其外接矩形
@@ -16,16 +17,20 @@ class Ship:
         self.rect = self.image.get_rect()
         # 每艘新飞船都放置在屏幕底部中央
         self.rect.midbottom = self.screen_rect.midbottom
+        # 飞船的属性中存一个浮点数
+        self.x = float(self.rect.x)
         # 飞船一开始不移动
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """根据标识移动飞船"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # 更新飞船而不是rect的值
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         """指定位置绘制飞船"""
